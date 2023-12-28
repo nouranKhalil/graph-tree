b285b02a0f15c01a4e990ced80809556964d0b47
RanaMohamed6720 <121067602+RanaMohamed6720@users.noreply.github.com>
2023-12-28 08:48:41 +0200

    Update BinaryTree.java

--- Lab3/BinaryTree.java
+++ Lab3/BinaryTree.java
@@ -1,81 +1,106 @@
+import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BinaryTree {
-	   Node root;
+    Node root;
 
-	    // Preorder traversal
-	    public void preorderTraversal(Node node) {
-	        if (node != null) {
-	            System.out.print(node.data + " ");
-	            preorderTraversal(node.left);
-	            preorderTraversal(node.right);
-	        }
-	    }
+    // Preorder traversal
+    public void preorderTraversal(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorderTraversal(node.left);
+            preorderTraversal(node.right);
+        }
+    }
 
-	    // Inorder traversal
-	    public void inorderTraversal(Node node) {
-	        if (node != null) {
-	            inorderTraversal(node.left);
-	            System.out.print(node.data + " ");
-	            inorderTraversal(node.right);
-	        }
-	    }
+    // Inorder traversal
+    public void inorderTraversal(Node node) {
+        if (node != null) {
+            inorderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inorderTraversal(node.right);
+        }
+    }
 
-	    // Postorder traversal
-	    public void postorderTraversal(Node node) {
-	        if (node != null) {
-	            postorderTraversal(node.left);
-	            postorderTraversal(node.right);
-	            System.out.print(node.data + " ");
-	        }
-	    }
+    // Postorder traversal
+    public void postorderTraversal(Node node) {
+        if (node != null) {
+            postorderTraversal(node.left);
+            postorderTraversal(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
 
-	    public static void main(String[] args) {
-	        BinaryTree tree = new BinaryTree();
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
 
-	      
-	        Scanner scanner = new Scanner(System.in);
+      
+        Scanner scanner = new Scanner(System.in);
 
-	        System.out.println("Enter the value of the root node:");
-	        int rootValue = scanner.nextInt();
-	        tree.root = new Node(rootValue);
+        try {
+            System.out.println("Enter the value of the root node:");
+            int rootValue = scanner.nextInt();
+            tree.root = new Node(rootValue);
 
-	        Queue<Node> queue = new LinkedList<>();
-	        queue.add(tree.root);
+            HashSet<Integer> uniqueValues = new HashSet<>();
+            uniqueValues.add(rootValue);
 
-	        while (!queue.isEmpty()) {
-	            Node currentNode = queue.poll();
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(tree.root);
 
-	            System.out.println("Enter left child value of " + currentNode.data + " (or -1 to skip):");
-	            int leftValue = scanner.nextInt();
-	            if (leftValue != -1) {
-	                currentNode.left = new Node(leftValue);
-	                queue.add(currentNode.left);
-	            }
+            while (!queue.isEmpty()) {
+                Node currentNode = queue.poll();
 
-	            System.out.println("Enter right child value of " + currentNode.data + " (or -1 to skip):");
-	            int rightValue = scanner.nextInt();
-	            if (rightValue != -1) {
-	                currentNode.right = new Node(rightValue);
-	                queue.add(currentNode.right);
-	            }
-	        }
+                try {
+                    System.out.println("Enter left child value of " + currentNode.data + " (or -1 to skip):");
+                    int leftValue = scanner.nextInt();
+                    if (leftValue != -1) {
+                        if (uniqueValues.contains(leftValue)) {
+                            System.out.println("Error: Duplicate value entered. Please enter a unique value.");
+                            System.exit(0);
+                        }
+                        currentNode.left = new Node(leftValue);
+                        uniqueValues.add(leftValue);
+                        queue.add(currentNode.left);
+                    }
 
-	        scanner.close();
+                    System.out.println("Enter right child value of " + currentNode.data + " (or -1 to skip):");
+                    int rightValue = scanner.nextInt();
+                    if (rightValue != -1) {
+                        if (uniqueValues.contains(rightValue)) {
+                            System.out.println("Error: Duplicate value entered. Please enter a unique value.");
+                            continue;
+                        }
+                        currentNode.right = new Node(rightValue);
+                        uniqueValues.add(rightValue);
+                        queue.add(currentNode.right);
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Error: Please enter a valid integer value.");
+                    System.exit(0);
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter a valid integer value for the root node.");
+        } finally {
+            scanner.close();
+        }
 
-	        // Perform traversals
-	        System.out.print("Preorder Traversal: ");
-	        tree.preorderTraversal(tree.root);
-	        System.out.println();
+        // Perform traversals
+        System.out.print("Preorder Traversal: ");
+        tree.preorderTraversal(tree.root);
+        System.out.println();
 
-	        System.out.print("Inorder Traversal: ");
-	        tree.inorderTraversal(tree.root);
-	        System.out.println();
+        System.out.print("Inorder Traversal: ");
+        tree.inorderTraversal(tree.root);
+        System.out.println();
 
-	        System.out.print("Postorder Traversal: ");
-	        tree.postorderTraversal(tree.root);
-	        System.out.println();
-	    }
+        System.out.print("Postorder Traversal: ");
+        tree.postorderTraversal(tree.root);
+        System.out.println();
+    }
 }
+
