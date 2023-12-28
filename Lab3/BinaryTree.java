b050409bc4883b4e4f2121598e3fd576f8d2d16b
RanaMohamed6720 <121067602+RanaMohamed6720@users.noreply.github.com>
2023-12-28 06:07:17 +0200

    Add files via upload

---
+++ Lab3/BinaryTree.java
@@ -0,0 +1,81 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BinaryTree {
+	   Node root;
+
+	    // Preorder traversal
+	    public void preorderTraversal(Node node) {
+	        if (node != null) {
+	            System.out.print(node.data + " ");
+	            preorderTraversal(node.left);
+	            preorderTraversal(node.right);
+	        }
+	    }
+
+	    // Inorder traversal
+	    public void inorderTraversal(Node node) {
+	        if (node != null) {
+	            inorderTraversal(node.left);
+	            System.out.print(node.data + " ");
+	            inorderTraversal(node.right);
+	        }
+	    }
+
+	    // Postorder traversal
+	    public void postorderTraversal(Node node) {
+	        if (node != null) {
+	            postorderTraversal(node.left);
+	            postorderTraversal(node.right);
+	            System.out.print(node.data + " ");
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        BinaryTree tree = new BinaryTree();
+
+	      
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter the value of the root node:");
+	        int rootValue = scanner.nextInt();
+	        tree.root = new Node(rootValue);
+
+	        Queue<Node> queue = new LinkedList<>();
+	        queue.add(tree.root);
+
+	        while (!queue.isEmpty()) {
+	            Node currentNode = queue.poll();
+
+	            System.out.println("Enter left child value of " + currentNode.data + " (or -1 to skip):");
+	            int leftValue = scanner.nextInt();
+	            if (leftValue != -1) {
+	                currentNode.left = new Node(leftValue);
+	                queue.add(currentNode.left);
+	            }
+
+	            System.out.println("Enter right child value of " + currentNode.data + " (or -1 to skip):");
+	            int rightValue = scanner.nextInt();
+	            if (rightValue != -1) {
+	                currentNode.right = new Node(rightValue);
+	                queue.add(currentNode.right);
+	            }
+	        }
+
+	        scanner.close();
+
+	        // Perform traversals
+	        System.out.print("Preorder Traversal: ");
+	        tree.preorderTraversal(tree.root);
+	        System.out.println();
+
+	        System.out.print("Inorder Traversal: ");
+	        tree.inorderTraversal(tree.root);
+	        System.out.println();
+
+	        System.out.print("Postorder Traversal: ");
+	        tree.postorderTraversal(tree.root);
+	        System.out.println();
+	    }
+}
